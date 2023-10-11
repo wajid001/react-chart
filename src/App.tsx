4954c4ec59ad0359d1b5d6ec40d929a70fda420c
wajid001 <wajiddev964@gmail.com>
2023-10-11 22:10:11 +0500

    added mui and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Grid } from "@mui/material";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Grid container>
+        <Grid item xs={8}>
+          xs=8
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
