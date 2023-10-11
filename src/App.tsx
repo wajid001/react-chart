4196cad4572a9b9fe0df9a7a1416958fcc204533
wajid001 <wajiddev964@gmail.com>
2023-10-11 22:59:20 +0500

    added chart and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
 import { Grid } from '@mui/material'
+import LineChart from './components/LineChart'
+import { data, options } from './constants/lineChart.constants'
+import OptionTabs from './components/OptionTabs'
 
 function App() {
   return (
     <div className="App">
       <Grid container>
         <Grid item xs={8}>
-          xs=8
+          <LineChart options={options} data={data} />
+          <OptionTabs />
         </Grid>
       </Grid>
     </div>
---
+++ src/components/LineChart.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line as LineChart } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export default LineChart
+
+// export function App() {
+//   return <Line options={options} data={data} />
+// }
